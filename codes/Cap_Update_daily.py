61fafc46c93c85dcaa319f02b5118a1f7b1c545c
JiageNG <heyujia@aliyun.com>
2019-08-28 22:55:54 +0800

    Update Cap_Update_daily.py

--- codes/Cap_Update_daily.py
+++ codes/Cap_Update_daily.py
@@ -3,7 +3,7 @@
 import pymysql
 
 def cap_update_daily(state_dt):
-    para_norisk = (1.0 + 0.025 / 365)
+    para_norisk = 0.025 / 365
     db = pymysql.connect(host='localhost', user='root', passwd='your password', db='your dbname', charset='utf8mb4')
     cursor = db.cursor()
     sql_pool = "select * from my_stock_pool"
@@ -24,11 +24,18 @@ def cap_update_daily(state_dt):
         else:
             print('Cap_Update_daily Err!!')
             raise Exception
-    sql_cap = "select * from my_capital order by seq asc"
-    cursor.execute(sql_cap)
-    done_cap = cursor.fetchall()
+    sql_cap1 = "select * from my_capital where state_dt <'%s' order by seq asc" % state_dt
+    cursor.execute(sql_cap1)
+    done_cap1 = cursor.fetchall()
     db.commit()
-    new_cash_cap = float(done_cap[-1][2]) * para_norisk
+
+    sql_cap2 = "select * from my_capital order by seq asc"
+    cursor.execute(sql_cap2)
+    done_cap2 = cursor.fetchall()
+    db.commit()
+
+    # 最近一笔REST和上一笔交易日所产生的货币资金收益
+    new_cash_cap = float(done_cap2[-1][2]) + float(done_cap1[-1][2]) * para_norisk
     new_total_cap = new_cash_cap + new_lock_cap
     sql_insert = "insert into my_capital(capital, money_lock, money_rest, bz, state_dt) values ('%.2f', '%.2f', '%.2f', '%s', '%s')" % (new_total_cap, new_lock_cap, new_cash_cap, str('Daily_Update'), state_dt)
     cursor.execute(sql_insert)
